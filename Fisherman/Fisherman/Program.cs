579300be5f5d1a69ec41dd64ed1b25b0c9be946f
Jia Yi Lim (1000142886) <LIMJ5@student.op.ac.nz>
2026-05-19 10:57:08 +1200

    Game intro coded

--- Fisherman/Fisherman/Program.cs
+++ Fisherman/Fisherman/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Numerics;
 using System.Text;
 
@@ -331,6 +332,34 @@ namespace Team_Fisherman
         }
 
 
+        //Game instruction here 
+        static void Wait()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("A violent storm swallows the sea.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("You - A simple fisherman, out alone on a late-night fishing trip. What began as a calm evening quickly turns into chaos as dark clouds cover the sky and enormous waves crash against your boat.");
+            Console.WriteLine("Just as you try to turn back, a massive wave smashes into the ship");
+            Thread.Sleep(2000);
+            Console.Clear() ;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(".....");
+            Console.WriteLine();
+            Console.WriteLine("Everything goes black.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
 
+        }
+
+        static void GameIntro()
+        {
+            Console.WriteLine();
+        }
     }
 }
