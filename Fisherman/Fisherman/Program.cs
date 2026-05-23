18048b341f458e9d5b5a61e2f547bc3d49b7891a
Zac Woods <WOODZP1@student.op.ac.nz>
2026-05-23 16:49:55 +1200

    Added the dodge code and created a section at the top for bugs and sugetions

--- Fisherman/Fisherman/Program.cs
+++ Fisherman/Fisherman/Program.cs
@@ -1,11 +1,11 @@
 
 
-﻿using System.Drawing;
+using System.Drawing;
 
 
-﻿
 
-﻿using System;
+
+using System;
 using System.Numerics;
 
 using System.Security;
@@ -14,6 +14,16 @@ using System.Text;
 
 using test_fish;
 using static System.Net.Mime.MediaTypeNames;
+/*Put suggestions/bugs here 
+ ln 1200 put a Console.Readline(); currently skips the text
+ ln 1132 put a new line char before "the". The word wraps in the console and becomes Th and e 
+ 
+ 
+ 
+ 
+ 
+ */
+
 
 
 namespace Team_Fisherman
@@ -64,9 +74,10 @@ namespace Team_Fisherman
 
 
             Menu();
-
+            
             Wait();
             GameIntro();
+
             while (true)
             {
                 path = Get_path(map_tile, path);
@@ -251,16 +262,19 @@ namespace Team_Fisherman
                 return false;
             else if (buffer[To_Index(coords, size)] == '~')
             {
+                Console.Clear();
                 Fishing();
                 return false;
             }
             else if (buffer[To_Index(coords, size)] == 'S')
             {
+                Console.Clear();
                 Shopping();
                 return false;
             }
             else if (buffer[To_Index(coords, size)] == 'm')
             {
+                Console.Clear();
                 Fighting();
                 return false;
             }
@@ -1027,13 +1041,146 @@ namespace Team_Fisherman
         }
         //Dodging();
 
-        //Put the code for dodging in here.
-        static void Dodging()
+        //code for dodging - returns an int depending on how much damage taken 0 dead 1 survived 2 perfect -1 something went wrong
+        static int Dodging()
         {
 
-            Console.WriteLine("Dodging");
-            Thread.Sleep(2000);
+
+            Console.CursorVisible = false;
+
+            // 10 rows by 31 columns grid.
+            // 0 = empty, 1 = obstacle present. 
+            int[,] grid = new int[10, 31];
+            int loopCounter = 0, playerPos = 15;
+            int score = 0, health = 3;
+            int gridXLimit = 31, gridYLimit = 9;
+            int xNumber = 3;
+            bool isRunning = true;
+            Random rand = new();
+
+            while (isRunning)
+            {
+                // Input Control 
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true).Key;
+
+                    if (key == ConsoleKey.A && playerPos > 0)
+                        playerPos--;
+                    if (key == ConsoleKey.D && playerPos < 30)
+                        playerPos++;
+                    while (Console.KeyAvailable)
+                        Console.ReadKey(true);
+                }
+                // Game Logic: Move obstacles down (Iterating TOP to BOTTOM to prevent double-moving) 
+                for (int i = gridYLimit; i >= 0; i--)
+                {
+                    for (int x = 0; x < gridXLimit; x++)
+                    {
+                        if (grid[i, x] == 1)
+                        {
+                            grid[i, x] = 0; // Clear current position 
+                            if (i < gridYLimit)
+                            {
+                                grid[i + 1, x] = 1; // Move down one row safely
+                            }
+                            else
+                            {
+                                score++; // Cleared the bottom row successfully
+                                if ((score % 50 == 0) && (score < 150))
+                                {
+                                    xNumber--;
+                                }
+                                if (score == 150)
+                                {
+                                    isRunning = false;
+                                }
+                            }
+                        }
+                    }
+                }
+                // Spawn Logic (drop an X at row 0)
+                if (score < 141)
+                {
+                    loopCounter++;
+                    if (loopCounter % xNumber == 0)
+                    {
+                        int spawnPos = rand.Next(0, gridXLimit);
+                        grid[0, spawnPos] = 1;
+                    }
+                }
+                // Collision Detection (Player is always on row 9) 
+                if (grid[9, playerPos] == 1)
+                {
+                    Console.Beep();
+                    health--;
+                    Thread.Sleep(100);
+
+                    if (health <= 0)
+                    {
+                        isRunning = false;
+                    }
+                    else
+                    {
+                        grid[9, playerPos] = 0; // Clear the obstacle so it doesn't instantly hit again
+                        score += 1;
+                    }
+                }
+                else
+                {
+                    // Rendering What will be displayed on the screen
+                    Console.SetCursorPosition(0, 0);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"X's missed: {score}\t\tHealth: {health}\n----------------------------------");
+
+                    for (int i = 0; i < gridYLimit + 1; i++)
+                    {
+                        for (int j = 0; j < gridXLimit; j++)
+                        {
+                            if (i == gridYLimit && j == playerPos)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write("P");
+                            }
+                            else if (grid[i, j] == 1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write("X");
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+
+            // Game Over Screen
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (health == 0)
+            {
+                Console.WriteLine($"You Died!\n\nPress Enter to exit...");
+                return 0;
+            }
+            else if ((health > 0) && (health < 3))
+            {
+                Console.WriteLine($"You Survived! Score: {score}\n\nPress Enter to exit...");
+                return 1;
+            }
+            else if (health == 3)
+            {
+                Console.WriteLine($"Perfect Dodge! Score: {score} Health: {health}\n\nPress Enter to exit...");
+                return 2;
+            }
+            Console.ReadLine();
+            return -1;
         }
+
 
 
         //Game instruction here 
@@ -1285,7 +1432,7 @@ namespace Team_Fisherman
                 Console.ReadLine();
             }
 
-            }
-
         }
+
+    }
 }
