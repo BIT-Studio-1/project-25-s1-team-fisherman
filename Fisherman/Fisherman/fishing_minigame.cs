84b3abcc347dd9774f4b4d50cd7792d2f66451c5
Juan Marcus Sumaya (1000136474) <SUMAJA1@student.op.ac.nz>
2026-06-02 10:54:02 +1200

    remove Activated Pity

--- Fisherman/Fisherman/fishing_minigame.cs
+++ Fisherman/Fisherman/fishing_minigame.cs
@@ -107,7 +107,7 @@ namespace test_fish
                 f = new Fish ("Memory Fish", "epic", 10f, 20f, 1000, 0.0f, ConsoleColor.Magenta);
 
                 pityCounter = 0;
-                Console.WriteLine("Activated Pity");
+                
             }
             else
             {
